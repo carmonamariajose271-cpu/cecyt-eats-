49df090fee80c861ecf8161cda66c5ae84a1f344
carmonamariajose271-cpu <carmonamariajose271@gmail.com>
2026-06-22 09:57:21 -0600

    Debug variables entorno

--- servidor/db.js
+++ servidor/db.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 const conexion = mysql.createPool({
   host:     process.env.DB_HOST     || 'localhost',
   port:     parseInt(process.env.DB_PORT) || 3306,
